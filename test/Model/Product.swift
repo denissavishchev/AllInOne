883a1d6b9b65b66e04c363c2c8578853e634f7d3
Denis Savishchev <denis.savishchev@gmail.com>
2024-10-30 22:59:28 +0100

    reviews scrollview

--- test/Model/Product.swift
+++ test/Model/Product.swift
@@ -41,9 +41,9 @@ struct Product: Codable, Identifiable, Hashable{
                 rating: 4.2,
                 count: 19,
                 review: [
-                    Review(name: "Denis", date: "24.10.28", description: "We really enjoyed the taste of this tea and certainly a very good price. However we have had many broken strings that has not happened with the Tetley tea bags that we usually have. Getting a teaspoon out to fish out the teabag is putting me off buying this product again", rating: 4.8),
-                    Review(name: "Leonard", date: "24.10.27", description: "This has been my favourite tea for quite a while. Nice to see it do well in the recommendations. As noted, it has a extremely slight fruity flavour, but certainly not herbal. Different to other black teas, but still a black tea.", rating: 4.9),
-                    Review(name: "Daniel", date: "24.09.28", description: "Lacks body. Initially slightly sweet, but left an unpleasant and lingering aftertaste.", rating: 4.5)
+                    Review(name: "Denis", date: "24.10.28", description: "We really enjoyed the taste of this tea and certainly a very good price. However we have had many broken strings that has not happened with the Tetley tea bags that we usually have. Getting a teaspoon out to fish out the teabag is putting me off buying this product again", rating: 5),
+                    Review(name: "Leonard", date: "24.10.27", description: "This has been my favourite tea for quite a while. Nice to see it do well in the recommendations. As noted, it has a extremely slight fruity flavour, but certainly not herbal. Different to other black teas, but still a black tea.", rating: 4),
+                    Review(name: "Daniel", date: "24.09.28", description: "Lacks body. Initially slightly sweet, but left an unpleasant and lingering aftertaste.", rating: 3)
                 ]
             )
         ),
@@ -64,9 +64,9 @@ struct Product: Codable, Identifiable, Hashable{
                 rating: 4.5,
                 count: 13,
                 review: [
-                    Review(name: "Denis", date: "24.10.28", description: "Came in today for the first time and my goodness I don’t think I could ever eat cookies anywhere else! They had a wonderful variety (including Gluten Free) and I’m sure you can’t go wrong with any choice that you make. They even had treats for my fur babies. Thank…", rating: 4.9),
-                    Review(name: "Leonard", date: "24.10.27", description: "Awesome fresh cookies. Support local!", rating: 4.9),
-                    Review(name: "Daniel", date: "24.09.28", description: "The company I work for uses their cookies for client thank you gifts. Our clients are always happy and love them. I signed up myself for the monthly cookie tin and it was the best decision! They are so good and I never know what I’m getting and makes me…", rating: 4.7)
+                    Review(name: "Denis", date: "24.10.28", description: "Came in today for the first time and my goodness I don’t think I could ever eat cookies anywhere else! They had a wonderful variety (including Gluten Free) and I’m sure you can’t go wrong with any choice that you make. They even had treats for my fur babies. Thank…", rating: 3),
+                    Review(name: "Leonard", date: "24.10.27", description: "Awesome fresh cookies. Support local!", rating: 4),
+                    Review(name: "Daniel", date: "24.09.28", description: "The company I work for uses their cookies for client thank you gifts. Our clients are always happy and love them. I signed up myself for the monthly cookie tin and it was the best decision! They are so good and I never know what I’m getting and makes me…", rating: 5)
                 ]
             )
         ),
@@ -87,9 +87,9 @@ struct Product: Codable, Identifiable, Hashable{
                 rating: 4.5,
                 count: 59,
                 review: [
-                    Review(name: "Denis", date: "24.10.28", description: "Good sized muffins, moist and soft, fluffy yet with density like an English cake. They're not great but they are good enough. They are sold without a paper cup because they are cooked in a metallic tray. I would recommend you ask a member of staff to heat yours in store to ensure you get the best experience.", rating: 4.8),
-                    Review(name: "Leonard", date: "24.10.27", description: "Just looking at them spoiled my appetite. Each is wrapped in plastic film with expiry date stickers stuck on. They were sticky, bready and chewy. What added some taste to them were their fillings. Hazelnut chocolate spread for the chocolate one and jam for the white one. The muffins were way too expensive for what you get. LBP 10,000 for the two.", rating: 4.9),
-                    Review(name: "Daniel", date: "24.09.28", description: "To create the best muffin, I would recommend the following: bake them in Starbucks paper cups for a better look; fill them with something sweet like the ones at Costa; make them a more satisfying size like those from Dunkin' Donuts and avoid Pain D'Or's old school paper cases.", rating: 4.5)
+                    Review(name: "Denis", date: "24.10.28", description: "Good sized muffins, moist and soft, fluffy yet with density like an English cake. They're not great but they are good enough. They are sold without a paper cup because they are cooked in a metallic tray. I would recommend you ask a member of staff to heat yours in store to ensure you get the best experience.", rating: 4),
+                    Review(name: "Leonard", date: "24.10.27", description: "Just looking at them spoiled my appetite. Each is wrapped in plastic film with expiry date stickers stuck on. They were sticky, bready and chewy. What added some taste to them were their fillings. Hazelnut chocolate spread for the chocolate one and jam for the white one. The muffins were way too expensive for what you get. LBP 10,000 for the two.", rating: 3),
+                    Review(name: "Daniel", date: "24.09.28", description: "To create the best muffin, I would recommend the following: bake them in Starbucks paper cups for a better look; fill them with something sweet like the ones at Costa; make them a more satisfying size like those from Dunkin' Donuts and avoid Pain D'Or's old school paper cases.", rating: 5)
                 ]
             )
         )
@@ -106,16 +106,17 @@ struct Characteristics: Codable{
     let carbohydrates: Double
 }
 
-struct Reviews: Codable{
+struct Reviews: Codable, Identifiable{
+    var id = UUID().uuidString
     let rating: Double
     let count: Int
     let review: [Review]
 }
 
-struct Review: Codable{
+struct Review: Codable, Identifiable{
     var id = UUID().uuidString
     let name: String
     let date: String
     let description: String
-    let rating: Double
+    let rating: Int
 }
--- test/View/ProductDetailView.swift
+++ test/View/ProductDetailView.swift
@@ -10,7 +10,7 @@ struct ProductDetailView: View {
                 HStack{
                     Rectangle().frame(height: 0)
                 }
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .frame(maxWidth: .infinity)
                 .background(Color(.lightBlue))
                 HStack{
                     HStack{
@@ -46,21 +46,21 @@ struct ProductDetailView: View {
                         .shadow(radius: 5)
                     Spacer()
                 }.padding(.leading, 12)
-                HStack{
-                    Image(product.countryImage)
-                        .resizable()
-                        .frame(width: 32, height: 32)
-                    Text(product.country)
-                    Spacer()
-                }.padding(.leading, 12)
-                HStack{
-                    Text("Description:")
-                    Spacer()
-                }
-                .padding(.leading, 12)
-                .padding(.top, 12)
-                Text(product.description)
-                    .padding(EdgeInsets(top: 4, leading: 12, bottom: 0, trailing: 12))
+                                HStack{
+                                    Image(product.countryImage)
+                                        .resizable()
+                                        .frame(width: 32, height: 32)
+                                    Text(product.country)
+                                    Spacer()
+                                }.padding(.leading, 12)
+                                HStack{
+                                    Text("Description:")
+                                    Spacer()
+                                }
+                                .padding(.leading, 12)
+                                .padding(.top, 12)
+                                Text(product.description)
+                                    .padding(EdgeInsets(top: 4, leading: 12, bottom: 0, trailing: 12))
                 VStack{
                     HStack{
                         Text("Basic characteristics:")
@@ -103,18 +103,81 @@ struct ProductDetailView: View {
                         }.padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
                         Text("\(product.characteristics.carbohydrates.formatted()) g")
                     }
-                    
-                }.padding(EdgeInsets(top: 4, leading: 12, bottom: 0, trailing: 12))
-            Spacer()
+                }
+                .padding()
+                VStack{
+                    HStack{
+                        Text("Reviews")
+                            .font(.system(size: 22))
+                            .bold()
+                            .foregroundColor(.black)
+                        Spacer()
+                        Text("\(product.reviews.count)")
+                            .font(.system(size: 22))
+                            .bold()
+                            .foregroundColor(.green)
+                    }
+                    .padding(EdgeInsets(top: 4, leading: 12, bottom: 0, trailing: 12))
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack{
+                            ForEach(product.reviews.review){ prod in
+                                ReviewItem(product: prod)
+                            }
+                        }
+                    }
+                    .contentMargins(.leading, 12, for: .scrollContent)
+                    }
+                    Spacer()
+                }
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color(.lightBlue))
             }
-            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
             .background(Color(.lightBlue))
+            
         }
-        .background(Color(.lightBlue))
-        
+    }
+    
+    #Preview {
+        ProductDetailView(product: Product.mocData()[2])
+    }
+
+struct ReviewItem: View {
+    
+    let product: Review
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            Text(product.name)
+            Text(product.date)
+            HStack{
+                ForEach(1..<6){ i in
+                    StarView(fill: fillingFor(i))
+                }
+            }
+            Spacer()
+            Text(product.description)
+        }
+        .padding(12)
+        .frame(width: 220, height: 200)
+        .background(Color(.systemMint))
+        .cornerRadius(12)
+    }
+    private func fillingFor(_ index: Int) -> Bool {
+        index <= product.rating
     }
 }
 
-#Preview {
-    ProductDetailView(product: Product.mocData()[2])
+struct StarView: View {
+    
+    let fill: Bool
+    
+    var body: some View {
+        Image(systemName: fill ? "star.fill" : "star")
+            .resizable()
+            .frame(width: 18, height: 18)
+            .foregroundStyle(.yellow)
+    }
 }
+    
+
+
